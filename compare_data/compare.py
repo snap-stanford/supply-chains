cc327f45271accd9d8e5efda611c5cd625b96146
Benjamin Yan <84124256+yanbenjamin@users.noreply.github.com>
2023-05-01 09:53:10 -0700

    Update compare.py

--- compare_data/compare.py
+++ compare_data/compare.py
@@ -5,6 +5,7 @@ import read_BACI
 import read_Hitachi
 import matplotlib.pyplot as plt 
 import numpy as np
+import scipy
 
 if __name__ == "__main__":
     
@@ -26,16 +27,38 @@ if __name__ == "__main__":
         if (sc_info["currency"] != 0 and globalised_info["currency"] != 0):
             weight_points.append((sc_info["currency"], globalised_info["currency"]))
         
-    #plot them
+    #plot currrency flow (x-axis being supply chain data, y-axis being the BACI global data)
     plt.scatter([c[0] for c in currency_points], [c[1] for c in currency_points], color = "green", s = 0.1)
     plt.yscale("log")
     plt.xscale("log")
-    plt.savefig("out1.jpg")
+    plt.xlabel("log(currency flow) in SC Data (in ?)", fontsize = 10)
+    plt.ylabel("log(currency flow) in BACI Data (in current USD)", fontsize = 10)
+    plt.title("Dataset Comparison for Global Product-Level Currency Flow", fontsize = 12)
+    plt.savefig("trade_currency.jpg")
     plt.show()
     
     plt.clf()
+    #plot shipping weight (x-axis being supply chain data, y-axis being the BACI global data)
     plt.scatter([c[0] for c in weight_points], [c[1] for c in weight_points], color = "blue", s = 0.1)
     plt.yscale("log")
     plt.xscale("log")
-    plt.savefig("out2.jpg")
+    plt.xlabel("log(total weight) in SC Data (in ?)", fontsize = 10)
+    plt.ylabel("log(total weight) in BACI Data (in tonnes)", fontsize = 10)
+    plt.title("Dataset Comparison for Global Product-Level Trade Weight", fontsize = 12)
+    plt.savefig("trade_weight.jpg")
     plt.show()
+    
+    #statistical testing 
+    currency_pearson_r = scipy.stats.pearsonr([c[0] for c in weight_points], [c[1] for c in weight_points]).statistic
+    weight_pearson_r = scipy.stats.pearsonr([c[0] for c in weight_points], [c[1] for c in weight_points]).statistic
+    
+    currency_spearman_r = scipy.stats.spearmanr([c[0] for c in weight_points], [c[1] for c in weight_points]).statistic
+    weight_spearman_r = scipy.stats.spearmanr([c[0] for c in weight_points], [c[1] for c in weight_points]).statistic
+    
+    print("Pearson Coefficient for Product Currency Flow", currency_pearson_r)
+    print("Pearson Coefficient for Product Shipping Weight", weight_pearson_r)
+    
+    print("Spearman Coefficient for Product Currency Flow", currency_spearman_r)
+    print("Spearman Coefficient for Product Shipping Weight", weight_spearman_r)
+    
+    #potentially look into log-scaled coefficients as well, and run scipy regression
