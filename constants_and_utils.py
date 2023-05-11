9c97b6f9a336d5efc146db4a67c60ee99f5ca4e5
Serina <serinac@stanford.edu>
2023-05-11 02:24:28 +0000

    simple notebook to test memory usage

--- constants_and_utils.py
+++ constants_and_utils.py
@@ -1,18 +1,22 @@
 import psutil
 from psutil._common import bytes2human
 
-def check_memory_usage():
+def check_memory_usage(print_message=True):
+    """
+    Checks current memory usage.
+    """
     virtual_memory = psutil.virtual_memory()
     total_memory = getattr(virtual_memory, 'total')
     available_memory = getattr(virtual_memory, 'available')
     free_memory = getattr(virtual_memory, 'free')
     available_memory_percentage = 100. * available_memory / total_memory
-    print('Total memory: %s; free memory: %s; available memory %s; available memory %2.3f%%' % (
-        bytes2human(total_memory),
-        bytes2human(free_memory),
-        bytes2human(available_memory),
-        available_memory_percentage))
-    return available_memory_percentage
+    if print_message:
+        print('Total memory: %s; free memory: %s; available memory %s; available memory %2.3f%%' % (
+            bytes2human(total_memory),
+            bytes2human(free_memory),
+            bytes2human(available_memory),
+            available_memory_percentage))
+    return available_memory
 
 def parse_battery_bom():
     """
