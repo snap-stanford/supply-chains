6dda2177a26ecf96c23ef834ab3ca29f00d95ed0
ZhiyinL <zhiyinl@stanford.edu>
2023-05-23 22:29:16 +0000

    [feat] Mega Analysis battery supplier with baseline and plots (deduplicated)

--- time_series.py
+++ time_series.py
@@ -204,11 +204,10 @@ def get_transaction_df(PROD='battery', tx_type='supplier', companies=[]):
     for hs in hs_codes:
         if len(companies) >= 1:
             tx_string = f"{tx_col} like '{companies[0]}'" + "".join([f" or {tx_col} like '{company}'" for company in companies[1:]])
-            query = f"select date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount, COUNT(*) as count, COUNT(DISTINCT id) as num_ids from logistic_data where (hs_code like '{hs}%' and {tx_string}) GROUP BY date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount;"
+            query = f"select date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount, COUNT(*) as count, COUNT(DISTINCT id) as num_ids from logistic_data where (hs_code like '{hs}%') and ({tx_string}) GROUP BY date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount;"
         else:
-            query = f"select date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount, COUNT(*) as count, COUNT(DISTINCT id) as num_ids from logistic_data where hs_code like '{hs}%' GROUP BY date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount;"
+            query = f"select date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount, COUNT(*) as count, COUNT(DISTINCT id) as num_ids from logistic_data where (hs_code like '{hs}%') GROUP BY date, supplier_id, buyer_id, hs_code, quantity, weight, price, amount;"
         df = rs.query_df(query)
-
         if df is not None:
             df = df.drop_duplicates()
             print(hs, '->', len(df), len(df.drop_duplicates()))
@@ -280,9 +279,9 @@ def get_transaction_time_df(SUPPLY_PROD='battery', BUY_PROD='bms', csv_name='sam
     buy_df = load_df(name=buy_name, csv_name=csv_name)
     print(len(supply_df), len(buy_df))
 
-    # # convert date to datetime
-    # supply_df['datetime'] = pd.to_datetime(supply_df.date)
-    # buy_df['datetime'] = pd.to_datetime(buy_df.date)
+    # convert date to datetime
+    supply_df['datetime'] = pd.to_datetime(supply_df.date)
+    buy_df['datetime'] = pd.to_datetime(buy_df.date)
 
     # extract month from date string
     supply_df['month'] = supply_df.date.apply(lambda x: x.rsplit('-', 1)[0])
@@ -292,13 +291,13 @@ def get_transaction_time_df(SUPPLY_PROD='battery', BUY_PROD='bms', csv_name='sam
     supply_df['month_datetime'] = pd.to_datetime(supply_df.month)
     buy_df['month_datetime'] = pd.to_datetime(buy_df.month)
 
-#     # extract week from datetime.dt
-#     supply_df['week'] = supply_df.datetime.dt.strftime("%G-%V-1")
-#     buy_df['week'] = buy_df.datetime.dt.strftime("%G-%V-1")
+    # extract week from datetime.dt
+    supply_df['week'] = supply_df.datetime.dt.strftime("%G-%V-1")
+    buy_df['week'] = buy_df.datetime.dt.strftime("%G-%V-1")
 
-#     # convert week to datetime
-#     supply_df['week_datetime'] = supply_df.week.apply(lambda x: datetime.date.fromisocalendar(int(x.rsplit('-', 2)[0]), int(x.rsplit('-', 2)[1]), 1))
-#     buy_df['week_datetime'] = buy_df.week.apply(lambda x: datetime.date.fromisocalendar(int(x.rsplit('-', 2)[0]), int(x.rsplit('-', 2)[1]), 1))
+    # convert week to datetime
+    supply_df['week_datetime'] = supply_df.week.apply(lambda x: datetime.date.fromisocalendar(int(x.rsplit('-', 2)[0]), int(x.rsplit('-', 2)[1]), 1))
+    buy_df['week_datetime'] = buy_df.week.apply(lambda x: datetime.date.fromisocalendar(int(x.rsplit('-', 2)[0]), int(x.rsplit('-', 2)[1]), 1))
     
     return supply_df, buy_df
 
@@ -374,7 +373,7 @@ def compare_sale_purchase_quantity_per_hscode(supply_df, buy_df, SUPPLY_PROD='ba
 
     summary_df = {}
     summary_df['group'] = csv_name
-
+    
     for supply_hs in supply_hs_codes:
         sub_supply_df = supply_df[supply_df.hs_code_str.str.contains(supply_hs)]
         summary_df[f"# of supply txn {supply_name}_{supply_hs}"] = len(sub_supply_df)
@@ -429,6 +428,98 @@ def compare_sale_purchase_quantity_per_hscode(supply_df, buy_df, SUPPLY_PROD='ba
         if not os.path.exists(outdir):
             os.mkdir(outdir)
         fig.savefig(f"./fig/{csv_name}/compare_sale_purchase_quantity_{supply_name}_{supply_hs}_{buy_name}_{time}_{csv_name}.jpg", bbox_inches="tight")
+
+    return summary_df
+
+def compare_sale_purchase_quantity_per_hscode_smooth(supply_df, buy_df, SUPPLY_PROD='battery', BUY_PROD='bms', time='monthly', num_before=3, num_after=3, csv_name='samsung'):
+    '''Calculate correlation for sale info (quantity) over time per supply hs_code, buy hs_code
+    Args:
+        time (str): supports 'daily', 'weekly', or 'monthly'
+    '''
+    supply_hs_codes = bom[SUPPLY_PROD] if SUPPLY_PROD in bom else SUPPLY_PROD
+    buy_hs_codes = bom[BUY_PROD] if BUY_PROD in bom else BUY_PROD
+    supply_name = SUPPLY_PROD if SUPPLY_PROD in bom else "baseline" 
+    buy_name = BUY_PROD if BUY_PROD in bom else "baseline" 
+    time_col = get_time_col(time)
+    
+    supply_df['hs_code_str'] = supply_df.hs_code.astype(str)  # convert HS code to str
+    buy_df['hs_code_str'] = buy_df.hs_code.astype(str)  # convert HS code to str
+
+    summary_df = {}
+    summary_df['group'] = csv_name
+    
+    for supply_hs in supply_hs_codes:
+        sub_supply_df = supply_df[supply_df.hs_code_str.str.contains(supply_hs)]
+        summary_df[f"# of supply txn {supply_name}_{supply_hs}"] = len(sub_supply_df)
+        if len(sub_supply_df) > THRESHOLD:
+            supply_summary = sub_supply_df.groupby(time_col).quantity.sum()
+            # Begin smoothing
+            smooth_values = apply_smoothing(supply_summary.values, num_before=num_before, num_after=num_after)
+            assert(len(supply_summary)==len(smooth_values))
+            supply_summary.replace(supply_summary.values, smooth_values, inplace=True)
+            supply_summary.fillna(0, inplace=True)
+            # End smoothing
+            fig, axes = plt.subplots(2, 1, figsize=(8, 7), sharex=True)
+            ax = axes[0]           
+            ax.set_title(f'{time} sales of {supply_hs}', fontsize=12)
+            ax.plot(supply_summary.index.values, supply_summary.values)
+            ax.set_ylabel('Total quantity', fontsize=12)
+            ax = axes[1]
+            ax.set_title(f'{time} purchases of {buy_name}', fontsize=12)
+            
+            summary_df[f"# of buy txn {buy_name}_all"] = len(buy_df)
+            if len(buy_df) > THRESHOLD:
+                buy_summary = buy_df.groupby(time_col).quantity.sum()
+                # Begin smoothing
+                smooth_values = apply_smoothing(buy_summary.values, num_before=num_before, num_after=num_after)
+                assert(len(buy_summary)==len(smooth_values))
+                buy_summary.replace(buy_summary.values, smooth_values, inplace=True)
+                buy_summary.fillna(0, inplace=True)
+                # End smoothing
+                merged = pd.merge(supply_summary.rename('x'), buy_summary.rename('y'), 
+                                          left_index=True, right_index=True, how='inner')
+                r, p = pearsonr(merged.x, merged.y)
+                summary_df[f"corr {supply_name}_{supply_hs}, {buy_name}_all smooth_{num_before}_{num_after}"] = r
+            else:
+                summary_df[f"corr {supply_name}_{supply_hs}, {buy_name}_all smooth_{num_before}_{num_after}"] = None
+                
+            
+            for buy_hs in buy_hs_codes:
+                sub_buy_df = buy_df[buy_df.hs_code_str.str.contains(buy_hs)]
+                summary_df[f"# of supply txn {buy_name}_{buy_hs}"] = len(sub_buy_df)
+                if len(sub_buy_df) > THRESHOLD:
+                    buy_summary = sub_buy_df.groupby(time_col).quantity.sum()
+                    # Begin smoothing
+                    smooth_values = apply_smoothing(buy_summary.values, num_before=num_before, num_after=num_after)
+                    assert(len(buy_summary)==len(smooth_values))
+                    buy_summary.replace(buy_summary.values, smooth_values, inplace=True)
+                    buy_summary.fillna(0, inplace=True)
+                    # End smoothing
+                    # merge to find common months
+                    merged = pd.merge(supply_summary.rename('x'), buy_summary.rename('y'), 
+                                      left_index=True, right_index=True, how='inner')
+                    r, p = pearsonr(merged.x, merged.y)
+                    print(buy_hs, len(sub_buy_df), 'r=%.3f (n=%d, p=%.3f)' % (r, len(merged), p)) # number of transaction, n is number of dates
+                    # normalize by mean to make comparison easier
+                    ax.plot(buy_summary.index.values, buy_summary.values / np.mean(buy_summary.values), label=buy_hs)
+                    summary_df[f"corr {supply_name}_{supply_hs}, {buy_name}_{buy_hs} smooth_{num_before}_{num_after}"] = r
+                else:
+                    print(buy_hs, len(sub_buy_df))
+                    summary_df[f"corr {supply_name}_{supply_hs}, {buy_name}_{buy_hs} smooth_{num_before}_{num_after}"] = None
+                
+            ax.legend(bbox_to_anchor=(1,1))
+            ax.set_ylabel('Total quantity (normalized)', fontsize=12)
+            plt.show()
+        else:
+            for buy_hs in buy_hs_codes:
+                summary_df[f"# of supply txn {buy_name}_{buy_hs}"] = None
+            print(supply_hs, len(sub_supply_df))
+            
+        fig.tight_layout()
+        outdir = f'./fig/{csv_name}/'
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        fig.savefig(f"./fig/{csv_name}/compare_sale_purchase_quantity_{supply_name}_{supply_hs}_{buy_name}_smooth_{num_before}_{num_after}_{csv_name}.jpg", bbox_inches="tight")
 
     return summary_df
 
