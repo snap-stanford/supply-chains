1f1851e44fe029d615c96331d4a2e7d876b3020d
Benjamin Yan <benjaminyan@Benjamins-MacBook-Air-8.local>
2023-06-29 18:22:03 -0700

    added BACI comparison code

---
+++ compare_data/README.md
@@ -0,0 +1,75 @@
+# Comparing Supply Chain Data with UN Global Trade Flows
+## Dependencies
+A base Conda environment should be sufficient, with the principal Python libraries being `matplotlib`, `numpy`, `scipy`, and `pandas`. For retrieving the Hitachi data, this code should be run on their `JupyterHub` server, interlaced with their remote `AWS ec2` data container and RedShift API.
+
+<figure>
+  <img src="chloropleth3.png" alt="Chloropleth map of global trade flows" style="width:60%; display:block; margin-left:auto, margin-right: auto">
+    <figcaption><em>Country exports and imports as computed in BACI data.</em></figcaption>
+</figure>
+
+## Data 
+The Hitachi supply chain data resides on the JupyterLab server. The BACI data (based on UN COMTRADE) is available at [dataset link](`http://www.cepii.fr/CEPII/en/bdd_modele/bdd_modele_item.asp?id=37`), with the most recent version HS17. You can either download and unzip all the BACI `.csv` files to the same directory, or run
+```zsh
+mkdir data
+mkdir data/BACI
+mkdir data/Hitachi
+
+python retrieve_data.py --baci_dir data/BACI --hitachi_dir data/Hitachi --rs_login <redshift username> <redshift password>
+```
+to obtain both the most recent Hitachi and BACI data, saved to folders `data/Hitachi` and `data/BACI`, respectively. 
+
+## Command Line Usage
+To compare the data (currency amounts and trade weights) in the year 2019 at the HS 6-digit product level, run
+```zsh
+mkdir images #create a directory to store plots
+python compare.py --year 2019 --hs_digits 6 --agg_type product --images_dir images
+```
+Dataset comparisons are available for years 2019, 2020, 2021, and the recommended HS digit levels are 2,4, and 6 (for ease of product interpretability). You can compare trade values under a different entity combination level by altering the value for`--agg_type`. 
+
+In general, it should take the form `[entity1]_[entity2]_ ...`, where each entity is among <b>[exporter,importer,product]</b> and ordered as such (i.e. `exporter_importer_product` is fine, but not `product_importer`). For instance, `--agg_type importer_product` will calculate an aggregated trade amount (in USD) and weight (in tonnes) for each pair of product and importer country, e.g. fermented black tea in the United States—and compare the metric reported in BACI versus Hitachi.
+
+This will also save out a scatterplot with Hitachi values on the `x`-axis and corresponding BACI values on the `y`-axis. The figure is saved under the directory specified in the Command.
+
+## API Usage
+### BACI Dataset
+For instance, if one wanted to compute the amount and weight of knitted / crocheted jerseys & cardigans (HS6 code = `611090`) imported by the US (ISO alpha-3 code = `USA`) in `2020` based on BACI,
+
+```python
+import read_BACI
+
+country_map, product_map, trade_flow_map = read_BACI.get_BACI_data(data_dir = "./data/BACI", aggregation_type = "importer_product", year = 2020, hs_level = 6)
+
+importer_country = "USA"
+product_code = 611090
+print(trade_flow_map[(importer_country, product_code)])
+
+"""output: {'currency': 209638476.0, 'weight': 5654.5960000000005}"""
+```
+
+Here, `country_map` maps from country IDs to names, `product_map` maps from HS codes to product descriptions, and `trade_flow_map` maps from entities (e.g. products, importer-product dyads, or exporter-importer-product tuples depending on `aggregation_type`) to amount (US dollar) and weight (tonnes) values. Also, `hs_level` represents how many Harmonized System digits to use for product classification (i.e. the level of product granularity).
+
+```python
+print(product_map[product_code])
+
+"""output: Jerseys, pullovers, cardigans, waistcoats and similar articles: of textile materials (other than wool or fine animal hair, cotton or man-made fibres), knitted or crocheted"""
+```
+
+### Hitachi Dataset
+For Hitachi, one would run an analagous script, although the `year` is provided differently (since the loader gives data from all years simultaneously). Note that both datasets may have certain products or entities missing.
+
+```python
+import read_Hitachi
+
+country_map, product_map, trade_flow_map = read_Hitachi.get_Hitachi_data(data_dir = "./data/Hitachi", aggregation_type = "exporter_product", hs_level = 6)
+
+#flexible iron or steel tubing
+product_code = 830710 
+exporter_country = "USA"
+
+trade_flows_2020 = trade_flow_map[2020]
+print(trade_flows_2020[(exporter_country,product_code)])
+
+"""output: {'weight': 16.330946196791054, 'currency': 150.03981445684792}"""
+```
+
+The above retrieves the values for flexible iron or steel tubing (hs6 code = `830710`) exported by the United States in `2020`. 
---
+++ compare_data/read_BACI.py
@@ -0,0 +1,173 @@
+""" utility functions for reading the BACI international
+trade dataset and aggregating quantities (e.g. weight, price). 
+"""
+
+import csv
+import json
+import os 
+import glob
+import sys
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import numpy as np
+
+def read_country_codes(csv_file = "./data/BACI/country_codes_V202301.csv"):
+    """
+    reads file on BACI country codes, mapping them to globally standardised ISO country codes
+    
+    Args:
+        csv_file (str): path to BACI country codes file
+    Returns:
+         dict: a lookup dictionary mapping the country code (key) to a value dictionary
+         composed of the country name and ISO 2-digit & 3-digit alpha representations. 
+    """
+    code2country = {}
+    with open(csv_file, "r") as file: 
+        reader = csv.reader(file)
+        for line_ix, line in enumerate(reader):
+            if (line_ix == 0): continue #header 
+            code, country_name_abb, country_name_full, iso_alpha2, iso_alpha3 = line 
+            code2country[int(code)] = {"name": country_name_full, "iso_alpha2": iso_alpha2, "iso_alpha3": iso_alpha3}
+        
+    return code2country
+
+def read_product_codes(csv_file = "./data/BACI/product_codes_HS17_V202301.csv"):
+    """
+    reads BACI file on product codes, mapping each HS6 code to its specific description
+    
+    Args:
+        csv_file (str): path to the product codes file
+    Returns:
+         dict: a lookup dictionary mapping the Harmonized System (HS) 6-digit codes found 
+               in the BACI data to their corresponding descriptions
+    """
+    code2product = {}
+    with open(csv_file,"r") as file:
+        reader = csv.reader(file)
+        for line_ix, line in enumerate(reader):
+            if (line_ix == 0): continue #header 
+            product_code, product_description = line 
+            code2product[int(product_code)] = product_description 
+            
+    return code2product
+
+def get_entity_constructor(aggregation_type, country_map):
+    """
+    given an aggregation type (e.g. exporter_importer, importer) and map from country IDs to 
+    global ISO codes, this returns a generator function to obtain the keys used for dataset 
+    aggregation (each key corresponds to a distinct entity within the aggregation scheme, 
+    such as USA or MEX under importer or tuple<USA, 100000> under importer_product)
+    
+    note: wanted to implement this with prettier code, but use of separate lambda functions
+    optimises performance speed noticeably 
+    """
+    if (aggregation_type == "exporter"):
+        return lambda exporter, importer, product: country_map[exporter]["iso_alpha3"]
+    if (aggregation_type == "importer"):
+        return lambda exporter, importer, product: country_map[importer]["iso_alpha3"]
+    if (aggregation_type == "product"):
+        return lambda exporter, importer, product: product
+    if (aggregation_type == "exporter_importer"):
+        return lambda exporter, importer, product: (country_map[exporter]["iso_alpha3"], country_map[importer]["iso_alpha3"])
+    if (aggregation_type == "exporter_product"):
+        return lambda exporter, importer, product: (country_map[exporter]["iso_alpha3"], product)
+    if (aggregation_type == "importer_product"):
+        return lambda exporter, importer, product: (country_map[importer]["iso_alpha3"], product)
+    if (aggregation_type == "exporter_importer_product"):
+        return lambda exporter, importer, product: (country_map[exporter]["iso_alpha3"], country_map[importer]["iso_alpha3"], product)
+    raise ValueError("invalid aggregation type")
+
+def aggregate(csv_file =  "./data/BACI/BACI_HS17_Y2018_V202301.csv", aggregation_type = "product", hs_level = 6, country_map = {}):
+    """
+    reads the BACI file on bilateral trade, aggregating under the specified entity combination (aggregation_type)
+    
+    Args:
+        csv_file (str): path to a yearly BACI trade file
+        aggregation_type (str): specifies the entity combinations for which we calculate trade flows.
+                                Should take the form [entity1]_[entity2]_ ..., where each entity is among 
+                               ["exporter","importer","product"] and ordered as such (e.g. no product_importer)
+        hs_level (int): the number of HS digits we use to represent products 
+                        i.e. level of product granularity. Should be in [2,4,6].
+        country_map (dict): maps from country id in the dataset to the standardised ISO codes. can be empty
+                        if not aggregating for countries (i.e. aggregation_type is product)
+        
+    Returns:
+         dict: an econometrics dictionary from entities (e.g. country, product; see aggregation_type) to
+                 aggregated currency flow (in current US dollars) and weight (in metric tonnes) in global trade
+             
+    """
+    df = pd.read_csv(csv_file)
+    n = len(df)
+    product_list, cash_list, weight_list = list(df["k"]), list(df["v"]), list(df["q"])
+    exporter_list, importer_list = list(df["i"]), list(df["j"]) 
+    entities_to_extract = aggregation_type.strip().split("_")
+    
+    econometrics_dict = {}
+
+    entity_extractor = get_entity_constructor(aggregation_type, country_map)
+    print(f"#### Aggregating Baci Data for HS{hs_level} at HS{hs_level} Entity Level {aggregation_type} ####")
+    for i in tqdm(range(n)):
+        product, currency, weight = product_list[i], cash_list[i], weight_list[i]
+        exporter, importer = exporter_list[i], importer_list[i]
+    
+        #in current USD dollars
+        currency = float(currency) * 1000 
+        #in metric tons
+        weight = float(weight) if "NA" not in weight else 0
+        #whether we consider 2-digit, 4-digit, or full 6-digit HS codes
+        product = product // 10**(6 - hs_level)
+        
+        hybrid_entity = entity_extractor(exporter, importer, product)
+        if hybrid_entity in econometrics_dict:
+            econometrics_dict[hybrid_entity]["currency"] += currency
+            econometrics_dict[hybrid_entity]["weight"] += weight
+        else:
+            econometrics_dict[hybrid_entity] = {"currency": currency, "weight": weight}
+             
+    return econometrics_dict
+
+def get_BACI_data(data_dir = "./data/BACI", aggregation_type = "product", year = 2020, hs_level = 6):
+    """
+    reads a BACI yearly file on bilateral trade, aggregating under the specified entity combination (aggregation_type)
+    
+    Args:
+        data_dir (str): path--preferably absolute--to the directory storing the BACI data
+        year (int): the year to extract bilateral trade data from (2017 - 2021, inclusive)
+        aggregation_type (str): species the entity combinations for which we calculate trade flows (see aggregate)
+                        
+    Returns:
+         tuple[dict]: Three dictionaries. (1) from internal county codes used in dataset to 
+                     globally standardised ISO codes, (2) from product HS6 codes to
+                     descriptions, (3) from hs6 product code to aggregated USD currency and 
+                     weight in global trade flows of that product
+    """
+    years_covered = [2017, 2018, 2019, 2020, 2021]
+    if (year not in years_covered): raise ValueError("year must be in 2017-2021")
+    
+    country_codes_file = os.path.join(data_dir, "country_codes_V202301.csv")
+    product_codes_file = os.path.join(data_dir, "product_codes_HS17_V202301.csv")
+    trade_data_file = os.path.join(data_dir, f"BACI_HS17_Y{year}_V202301.csv")
+          
+    country_map = read_country_codes(country_codes_file)
+    product_map = read_product_codes(product_codes_file)
+    trading_map = aggregate(trade_data_file, aggregation_type, hs_level, country_map)
+    
+    return country_map, product_map, trading_map
+    
+if __name__ == "__main__":
+    """
+    for testing out the functions above and printing a results snippet
+    """
+    aggregation_type = "exporter_importer_product" if (len(sys.argv) == 1) else sys.argv[1]
+    entities = aggregation_type.strip().split("_")
+    country_map, product_map, trading_map = get_BACI_data("/home/jamin/supply-chains/data/BACI", 
+                                                          aggregation_type = aggregation_type, year = 2020, hs_level = 6)
+    keys = list(trading_map.keys())
+    print(f"Number of Unique Keys under Entity Combination {aggregation_type}: {len(keys)}")
+    for i in np.random.choice(range(len(trading_map)), size = 20, replace = False):
+        key_name = tuple(product_map[entity] if entities[idx] == "product" else entity for idx, entity in enumerate(keys[i]))
+        print(key_name, trading_map[keys[i]])
+    
+    
+    
