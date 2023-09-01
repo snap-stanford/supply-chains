d478eaba738f578e0f0a6c7e7c0882317b1f8645
Serina Chang <serinac@stanford.edu>
2023-09-01 15:36:17 -0700

    leave out sigmoid since BCEWithLogitsLoss already combines Sigmoid with BCELoss

--- TGB/modules/decoder.py
+++ TGB/modules/decoder.py
@@ -21,10 +21,10 @@ class LinkPredictorTGNPL(torch.nn.Module):
         self.lin_prod = Linear(in_channels, in_channels)
         self.lin_final = Linear(in_channels, 1)
 
-    def forward(self, z_src, z_prod, z_dst):
+    def forward(self, z_src, z_dst, z_prod):
         h = self.lin_src(z_src) + self.lin_dst(z_dst) + self.lin_prod(z_prod)
         h = h.relu()
-        return self.lin_final(h).sigmoid()
+        return self.lin_final(h)
 
 
 class LinkPredictor(torch.nn.Module):
