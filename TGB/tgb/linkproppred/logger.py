539ce6359634e13a90cfc6a3a04a9f625dda65e3
Frank Kloster <frank.kloster@hal.hitachi.com>
2023-10-10 21:51:02 +0000

    Added Tensorboard logger.

---
+++ TGB/tgb/linkproppred/logger.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+import tensorboard
+from torch.utils.tensorboard import SummaryWriter
+import wandb
+
+class LoggerMixin(ABC):
+    def __init__(self):
+        self.logged_metrics = ["loss", "logits_loss", "inv_loss", "perf_metric_val"]
+
+    @abstractmethod
+    def log(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def __del__(self):
+        pass
+
+class TensorboardLogger(LoggerMixin):
+    def __init__(self):
+        self.writer = SummaryWriter()
+        self.logged_metrics = ["loss", "logits_loss", "inv_loss", "perf_metric_val"]
+        self._n_iter = 0
+
+    def log(self, metrics):
+        for metric in self.logged_metrics:
+            self.writer.add_scalar(f"{metric}",  metrics[metric], self._n_iter)
+        self._n_iter += 1
+
+    def __del__(self):
+        self.writer.close()
+        
+
+class WandbLogger(LoggerMixin):
+    def __init__(self, num_neighbors=10, model_name='tgnpl', wandb_project="model-experiments", wandb_team="snap", *args):
+        super().__init__()
+        wandb.init(
+            # set the wandb project where this run will be logged
+            project=wandb_project,
+            entity=wandb_team,
+            resume="allow",
+            # track hyperparameters and run metadata
+            config=args
+        )
+        self.config = wandb.config
+        wandb.summary["num_neighbors"] = num_neighbors
+        wandb.summary["model_name"] = model_name
+
+    def log(self, mode='training', **kwargs):
+        wandb.log({f"{mode}_{metric}": kwargs[metric] for metric in self.logged_metrics})
+
+    def __del__(self):
+        # pass
+        wandb.finish()
