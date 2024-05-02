d710cf71cc73e3e5c1157d2a801a517bc6e60a80
Serina Chang <serinac@stanford.edu>
2024-05-02 08:04:54 -0700

    replaced 'tesla' with 'sem'

--- register_data/preprocess_sem.py
+++ register_data/preprocess_sem.py
@@ -63,26 +63,26 @@ def get_company_idname_map(df):
 
     return company_id2name_final
   
-def preprocess_tesla(df, start_date = "2019-01-01", use_titles = True, id2company = None,
-                    length_timestamps = 1):
-    #deduplicate the Tesla dataset according to the primary key
-    df_tesla = df[PRIMARY_KEY]
-    df_tesla = df_tesla.drop_duplicates(subset = PRIMARY_KEY)
+def preprocess_sem(df, start_date = "2019-01-01", use_titles = True, id2company = None,
+                   length_timestamps = 1):
+    #deduplicate the SEM dataset according to the primary key
+    df_sem = df[PRIMARY_KEY]
+    df_sem = df_sem.drop_duplicates(subset = PRIMARY_KEY)
     
     #calculate the time stamps, as well as siphon out incorrectly categorized products (HS6)
-    data_to_ts = {date: get_ts(start_date, date, length_timestamps) for date in set(df_tesla["date"])}
-    # df_tesla["bill"] = 1
-    df_tesla["time_stamp"] = df_tesla["date"].apply(lambda date: data_to_ts[date])
-    df_tesla["hs6"] = df_tesla["hs_code"].apply(lambda hs_code: str(hs_code)[:6])
-    df_tesla = df_tesla[df_tesla["hs6"].str.match('^(?!00)[0-9]{6}')] 
+    data_to_ts = {date: get_ts(start_date, date, length_timestamps) for date in set(df_sem["date"])}
+    # df_sem["bill"] = 1
+    df_sem["time_stamp"] = df_sem["date"].apply(lambda date: data_to_ts[date])
+    df_sem["hs6"] = df_sem["hs_code"].apply(lambda hs_code: str(hs_code)[:6])
+    df_sem = df_sem[df_sem["hs6"].str.match('^(?!00)[0-9]{6}')] 
     
     #aggregate the transactions by time stamps, and sort by ascending time stamp 
-    df_tesla_agg = df_tesla.groupby(by = AGGREGATION_KEY).sum(numeric_only = True).reset_index()
-    df_tesla_agg = df_tesla_agg.rename(columns = {"quantity_sum": "total_quantity",
+    df_sem_agg = df_sem.groupby(by = AGGREGATION_KEY).sum(numeric_only = True).reset_index()
+    df_sem_agg = df_sem_agg.rename(columns = {"quantity_sum": "total_quantity",
                                                   "weight_sum": "total_weight",
                                                   "amount_sum": "total_amount"})
-    # df_tesla_agg = df_tesla_agg.drop(columns = ["price"])
-    df_tesla_agg = df_tesla_agg.sort_values(by = ["time_stamp"], ascending = True)
+    # df_sem_agg = df_sem_agg.drop(columns = ["price"])
+    df_sem_agg = df_sem_agg.sort_values(by = ["time_stamp"], ascending = True)
     
     #replace the company hashed IDs to the company names if requested 
     if (use_titles == True):
@@ -91,24 +91,24 @@ def preprocess_tesla(df, start_date = "2019-01-01", use_titles = True, id2compan
                                        "company_t": [id2company[id] for id in company_ids]})
 
         #replace the company supplier IDs
-        df_tesla_agg = pd.merge(df_tesla_agg, df_companies, 
+        df_sem_agg = pd.merge(df_sem_agg, df_companies, 
                                 left_on = "supplier_id", right_on = "company_id", how = "left")
-        df_tesla_agg = df_tesla_agg.rename(columns = {"company_t": "supplier_t"}).drop(
+        df_sem_agg = df_sem_agg.rename(columns = {"company_t": "supplier_t"}).drop(
                                 columns = {"company_id","supplier_id"})
         #replace the company buyer IDs
-        df_tesla_agg = pd.merge(df_tesla_agg, df_companies, left_on = "buyer_id", 
+        df_sem_agg = pd.merge(df_sem_agg, df_companies, left_on = "buyer_id", 
                                 right_on = "company_id", how = "left")
-        df_tesla_agg = df_tesla_agg.rename(columns = {"company_t": "buyer_t"}).drop(
+        df_sem_agg = df_sem_agg.rename(columns = {"company_t": "buyer_t"}).drop(
                                 columns = {"company_id","buyer_id"})
         
         #reorder the columns in the dataframe
-        df_tesla_agg = df_tesla_agg[["time_stamp","supplier_t","buyer_t","hs6","bill_count",
+        df_sem_agg = df_sem_agg[["time_stamp","supplier_t","buyer_t","hs6","bill_count",
                                     "total_quantity","total_amount","total_weight"]]
     else:
-        df_tesla_agg = df_tesla_agg[["time_stamp","supplier_id","buyer_id","hs6","bill_count",
+        df_sem_agg = df_sem_agg[["time_stamp","supplier_id","buyer_id","hs6","bill_count",
                                     "total_quantity","total_amount","total_weight"]]
     
-    return df_tesla_agg
+    return df_sem_agg
 
 
 if __name__ == "__main__":
@@ -122,10 +122,10 @@ if __name__ == "__main__":
     print("After removing 7 negative amount_sum rows, there are {} raw entries\n".format(len(df)))
     
     map_id2company = get_company_idname_map(df)
-    df_tesla = preprocess_tesla(df, args.start_date, args.use_titles, map_id2company,
-                               length_timestamps = args.length_timestamps)
-    df_tesla.to_csv(args.out_filepath, index = False)
+    df_sem = preprocess_sem(df, args.start_date, args.use_titles, map_id2company,
+                              length_timestamps = args.length_timestamps)
+    df_sem.to_csv(args.out_filepath, index = False)
     
-    print("Saved out {} transactions to {}!".format(len(df_tesla), args.out_filepath))
-    print(df_tesla.head(5))
-    print(df_tesla.tail(5))
+    print("Saved out {} transactions to {}!".format(len(df_sem), args.out_filepath))
+    print(df_sem.head(5))
+    print(df_sem.tail(5))
