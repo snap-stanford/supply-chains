small changes to deal with gradient bug

TGB/examples/linkproppred/general/model_experiments.py
@@ -207,7 +207,7 @@ def get_y_pred_for_batch(batch, model, neighbor_loader, data, device,
         no_parts = torch.isclose(caps, neg_ones)  
         assert (caps[~no_parts] >= 0).all()
         skip_amt = to_skip | no_parts | torch.isclose(caps, torch.zeros_like(caps))  # zero suggests incorrect prediction
-        caps = (torch.log(caps)-AMT_MEAN)/AMT_STD  # apply log and standard scaling, -1 will become nan
+        caps[~skip_amt] = (torch.log(caps[~skip_amt])-AMT_MEAN)/AMT_STD  # apply log and standard scaling
         caps = caps.reshape(-1, 1)
         
         if MODEL_NAME == 'INVENTORY':

TGB/modules/inventory_module.py
@@ -115,12 +115,14 @@ class TGNPLInventory(torch.nn.Module):
         inventories = self.inventory[src]
         assert inventories.shape == (len(src), self.num_prods)
         max_amt = inventories / torch.clip(parts_per_prod, 1e-5, None)  # so we don't divide by 0
-        max_val = torch.max(max_amt)
+        multiplier = torch.ones_like(max_amt)
         ispart = parts_per_prod > 0
-        max_amt[~ispart] = max_val  # set to max val so it doesn't affect min
+        max_val = torch.max(max_amt)
+        multiplier[~ispart] = max_val
+        max_amt = max_amt * multiplier  # set non-part to max val so it doesn't affect min
         caps, _ = torch.min(max_amt, dim=1)
         num_parts = (parts_per_prod > 0).sum(axis=1)  # has no parts, exogenous
-        caps[num_parts == 0] = -1
+        caps[num_parts == 0] = -1  # can't make prediction for exogenous product
         return caps
         
     def detach(self):